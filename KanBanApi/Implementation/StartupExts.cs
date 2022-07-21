4552190016bdb5067611bbf70c21f26a948dacc7
powercreek <micecd@gmail.com>
2022-07-21 16:51:41 -0500

    Add project files.

---
+++ KanBanApi/Implementation/StartupExts.cs
@@ -0,0 +1,17 @@
+﻿namespace KanBanApi.Implementation
+{
+    public static class StartupExts
+    {
+
+        public static IServiceCollection ImplementApi(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IConfiguration SetConfiguration(this IConfiguration configuration)
+        {
+            return configuration;
+        }
+
+    }
+}
